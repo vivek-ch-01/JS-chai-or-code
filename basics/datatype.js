35bd8824a4d79001ba077eb2e8adb7b30bcf90af
vivek-ch-01 <vivekkatoira01@gmail.com>
2025-08-04 13:49:34 +0000

    done datatypes.

---
+++ basics/datatype.js
@@ -0,0 +1,14 @@
+let a = null;
+let b = 343;
+let c = true;
+let d = BigInt("540") + BigInt("5");
+let e = "vivek"
+let f = Symbol("hello guys")
+
+console.log(a,b,c,d,e,f,);
+console.log(typeof c);
+
+console.log(typeof undefined);          //undefined
+console.log(typeof null);               //object
+
+console.log(typeof "vivek");
