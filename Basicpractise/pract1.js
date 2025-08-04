ce24dfb56872fc2a770698e38754d18d6e852299
vivek-ch-01 <vivekkatoira01@gmail.com>
2025-08-04 15:45:00 +0000

    done

---
+++ Basicpractise/pract1.js
@@ -0,0 +1,6 @@
+//Q.1 Create a variable of type string and try to add a number to it.
+//Q.2 Use type operator to find the datatype of the string.
+let x = "vivek"
+let y = 5
+console.log(x+y)
+console.log(typeof (x+y))
