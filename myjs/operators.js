87d4710f17a60b90d4ff828b5e3d91a069f7152a
vivek-ch-01 <vivekkatoira01@gmail.com>
2025-08-04 19:43:40 +0000

    donee

---
+++ myjs/operators.js
@@ -0,0 +1,44 @@
+// Arithmrtic operator---
+let a = 10;
+let b = 4;
+console.log("a+b = ", a+b)
+
+console.log("a**b = ",a**b)  // it meanse a to the power b
+
+console.log("a%b = ",a%b)    // it means the reminder when dividing a from b.
+
+console.log("a++ = ",a++)   // a= 10,  and here output of a++ is 10
+console.log("a = ",a)       //here output of a is 11
+
+console.log("++a = ",++a)   //here output of ++a is 12
+console.log("a = ",a)       //here output of a is 12
+
+// a++ means first the value of a is print.after that it is increased by 1.
+// ++a means first the value of a is increased by 1 then after it print thatincreased value
+
+// Assignment operator in javascript----
+
+let x = 5;
+x +=2       // x+2
+console.log(x)
+
+// Comparision Operators----
+
+let p = 5;
+let q = 8;
+console.log("p==q ",p==q)       //equal to
+console.log("p!=q ",p!=q)       //not equal to
+console.log("p===q ",p===q)     //equal value and type
+console.log("p!==q ",p!==q)     //not equal value or not equal type
+console.log("p>=q" ,p>=q)       //greater than or equal to
+
+// Logical Operators-----
+
+let m = 5;
+let n = 6;
+console.log(m<n && m==5)        //dono ke dono conditin sahi hone par hi output true aayega
+console.log(m<n || n==5)        //dono me se koi ek true hone par output true aayega
+console.log(!false)
+console.log(!true)
+
+// use ctrl + / to convert a selected line or paragraph to comments..
