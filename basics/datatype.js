1db8e13990895f8682b7dc9a6884ed9367ba97e5
vivek-ch-01 <vivekkatoira01@gmail.com>
2025-08-04 14:33:32 +0000

    done both

--- basics/datatype.js
+++ basics/datatype.js
@@ -1,9 +1,12 @@
+//there are 7 type of primitive datatypes...
+//null,number,bigint,boolean,symbol,string,undefined
 let a = null;
 let b = 343;
 let c = true;
 let d = BigInt("540") + BigInt("5");
 let e = "vivek"
 let f = Symbol("hello guys")
+let g = undefined
 
 console.log(a,b,c,d,e,f,);
 console.log(typeof c);
---
+++ basics/object.js
@@ -0,0 +1,11 @@
+// Object is non primitives datatypes
+// Objects in javascript is like dictinoary in phython.
+// Object means key & value in a pair.
+const item = {
+    "vivek": true,
+    "subh": false,
+    "LOvish": 45,
+    "Rohan": undefined
+} 
+
+console.log(item["vivek"])
