90ff10216232cc607352e6157d7064d0d3a888b2
vivek-ch-01 <vivekkatoira01@gmail.com>
2025-08-05 13:00:01 +0000

    datatype conversion

---
+++ myjs/dataconv.js
@@ -0,0 +1,28 @@
+let score = "33"
+
+console.log(typeof score);
+console.log(typeof (score));
+
+let valueinnumber   = Number(score)
+console.log(typeof valueinnumber);
+
+let x = 25
+let stringnum = String(x)
+console.log(stringnum);
+
+let named = "vivek"
+let booln = Boolean(named)
+console.log(booln)
+
+let m = ""
+let bool = Boolean(m)
+console.log(bool)
+
+// Conversation of different datatypes..
+//34 -- 33
+//33abc -- NAN(not a number)
+//abc -- NAN
+//true -- 1
+//false -- 0
+
+
