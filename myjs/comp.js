29fe9b1fed9f95e80bab91e221ba032a38361c64
vivek-ch-01 <vivekkatoira01@gmail.com>
2025-08-05 15:44:07 +0000

    done...

---
+++ myjs/comp.js
@@ -0,0 +1,12 @@
+// These type of comparion confuse us. So we Avoid them
+
+console.log(null>0);
+console.log(null==0);
+console.log(null>=0);
+
+console.log(undefined>0);
+console.log(undefined<0);
+console.log(undefined==0);
+
+
+
