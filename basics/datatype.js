f41d2028fefd03f8265aff5edc267dbf990a9189
vivek-ch-01 <vivekkatoira01@gmail.com>
2025-08-05 17:10:00 +0000

    datatypefullnote

--- basics/datatype.js
+++ basics/datatype.js
@@ -8,6 +8,12 @@ let e = "vivek"
 let f = Symbol("hello guys")
 let g = undefined
 
+// use of symbol datatypes---
+const id = Symbol('123')
+const anotherid = Symbol('123')
+console.log(id==anotherid)
+
+
 console.log(a,b,c,d,e,f,);
 console.log(typeof c);
 
@@ -15,3 +21,20 @@ console.log(typeof undefined);          //undefined
 console.log(typeof null);               //object
 
 console.log(typeof "vivek");
+
+
+// Non promitive datatypes---- ARRAY, OBJECTS, FUNCTIONS.
+
+const heros = ["superman","spiderman","antman"];        // Array
+
+let myobj = {
+    name:"vivek",                                       //Objects
+    age: 25,
+}
+
+const myfun = function(){
+    console.log("hello world");                         //Function
+}
+
+
+
