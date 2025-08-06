e4c24f6771b0e0c31acfe032d735bfc0b16b0607
vivek-ch-01 <vivekkatoira01@gmail.com>
2025-08-06 17:24:11 +0000

    dne

---
+++ myjs/nums.js
@@ -0,0 +1,17 @@
+const score = 400
+console.log(score)
+
+const balance = new Number(100)
+console.log(balance);
+
+//methods for NUMBER-----
+
+console.log(balance.toString().length);
+console.log(balance.toFixed(2));
+
+const othernumber = 123.8892
+console.log(othernumber.toPrecision(4));
+
+const hundred = 1000000
+console.log(hundred.toLocaleString('en-IN'));
+
